06c1f35170dba6010513fb660908a51c06c49976
muzzy_al <mustafa.almoozany@gmail.com>
2025-04-26 10:56:40 +0000

    Adding security key decoding and use in git hub actions and service account permissions in tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,21 @@
+resource "google_service_account" "build_admin" {
+  account_id   = "build-admin"
+  display_name = "Service account for Terraform builds"
+  project      = var.project_id
+}
+
+resource "google_project_iam_member" "build_admin_storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.build_admin.email}"
+}
+
+resource "google_project_iam_member" "build_admin_editor" {
+  project = var.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.build_admin.email}"
+}
+
 module "project_services" {
   source     = "./modules/project_services"
   project_id = var.project_id
