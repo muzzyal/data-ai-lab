6b866d417fd2d6542485884efd7363a472551324
muzzy_al <mustafa.almoozany@gmail.com>
2025-04-26 10:07:35 +0000

    Terraform fmt fixing

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 module "project_services" {
   source     = "./modules/project_services"
   project_id = var.project_id
-  services = local.required_services
+  services   = local.required_services
 }
