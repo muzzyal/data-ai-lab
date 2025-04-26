922053d3ace1929040cee7afdda4a4dac8ad0aa2
muzzy_al <mustafa.almoozany@gmail.com>
2025-04-26 10:01:31 +0000

    Terraform set up and github actions initial config

--- infra/modules/project_services/variables.tf
+++ infra/modules/project_services/variables.tf
@@ -2,7 +2,6 @@ variable "project_id" {
   description = "GCP Project ID"
   type        = string
 }
-
 variable "services" {
   description = "List of APIs (services) to enable"
   type        = list(string)
--- infra/variables.tf
+++ infra/variables.tf
@@ -10,3 +10,11 @@ variable "project_id" {
   description = "GCP Project ID"
   type        = string
 }
+variable "default_region" {
+  description = "Default GCP region"
+  type        = string
+}
+variable "default_zone" {
+  description = "Default GCP zone"
+  type        = string
+}
