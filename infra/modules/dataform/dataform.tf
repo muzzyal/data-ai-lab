0fa6b0bcb91b83b65c573f250598c1011044dd85
muzzy_al <mustafa.almoozany@gmail.com>
2025-05-16 22:28:53 +0000

    Deploying dataform repository

--- infra/modules/dataform/dataform.tf
+++ infra/modules/dataform/dataform.tf
@@ -6,18 +6,18 @@ module "github_token_secret" {
   members    = var.secret_members
 }
 
-# resource "google_dataform_repository" "dataform_repository" {
-#   provider = google-beta
-#   name = "data_ai_lab_repo"
-#   display_name = "data_ai_lab_repo"
+resource "google_dataform_repository" "dataform_repository" {
+  provider     = google-beta
+  name         = "data_ai_lab_repo"
+  display_name = "data_ai_lab_repo"
 
-#   git_remote_settings {
-#       url = var.git_repo_url
-#       default_branch = "main"
-#       authentication_token_secret_version = "${module.github_token_secret.secret_id}/versions/latest"
-#   }
+  git_remote_settings {
+    url                                 = var.git_repo_url
+    default_branch                      = "main"
+    authentication_token_secret_version = "${module.github_token_secret.secret_id}/versions/latest"
+  }
 
-#   depends_on = [
-#     moduel.github_token_secret
-#   ]
-# }
+  depends_on = [
+    moduel.github_token_secret
+  ]
+}
