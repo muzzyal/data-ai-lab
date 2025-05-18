cc011ab04074426c4dffc715a36407d710faff7b
muzzy_al <mustafa.almoozany@gmail.com>
2025-05-18 10:13:29 +0000

    Removing dataform directory as initiasation of workspace creates definitions directory

--- infra-init/main.tf
+++ infra-init/main.tf
@@ -16,6 +16,12 @@ resource "google_project_iam_member" "build_admin_editor" {
   member  = "serviceAccount:${google_service_account.build_admin.email}"
 }
 
+resource "google_project_iam_member" "project_iam_admin" {
+  project = var.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "serviceAccount:${google_service_account.build_admin.email}"
+}
+
 resource "google_project_iam_member" "secret_manager_admin" {
   project = var.project_id
   role    = "roles/secretmanager.admin"
