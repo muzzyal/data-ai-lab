240bde3062f6f3dee709bbbc7242b2aaf0ab4a32
muzzy_al <mustafa.almoozany@gmail.com>
2025-06-18 06:57:07 +0000

    Fixing bandit failures

--- cloud-dock/playground_stream_ingest/src/main.py
+++ cloud-dock/playground_stream_ingest/src/main.py
@@ -1,4 +1,4 @@
 from playground_stream_ingest.src.app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)  # nosec
