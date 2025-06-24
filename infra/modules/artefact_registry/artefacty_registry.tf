74add4ac5baea5050bb88da1f51f541295edae07
muzzy_al <mustafa.almoozany@gmail.com>
2025-06-24 06:46:09 +0000

    Artefact registry deployment

--- infra/modules/artefact_registry/artefacty_registry.tf
+++ infra/modules/artefact_registry/artefacty_registry.tf
@@ -1,4 +1,4 @@
-resource "google_artifact_registry_repository" "my-repo" {
+resource "google_artifact_registry_repository" "cloud_dock_repository" {
   location      = var.region
   repository_id = var.repository_id
   description   = var.description
